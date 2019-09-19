f49372ac61198aebb732fd6261d16e30cc7d021e
Lindsay <lindsay.lbm@gmail.com>
2019-09-19 15:22:35 -0500

    letteers only apply to game; correct guess array does not push to viewable html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 var wordOptions = ["adventure", "animals", "backpack", "campground", "campfire", "cabin", "compass", "equipment", "flashlight", "fishing", "forest", "hammock", "hike", "hiking boots", "bug spray", "lantern", "nature", "outdoors", "outside", "sleeping bag", "sunscreen", "tent", "water bottle", "waterfall"];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 // var buildWord;
 var currentWord;
 var userGuess;
@@ -31,24 +32,25 @@ document.onkeyup = function () {
     var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
     console.log(userGuess)
 
+    if (letters.indexOf(userGuess) > -1) {
 
+        //replace placeholder with chosen word
+        if (currentWord.indexOf(userGuess) > -1) {
+            correctGuess.push(userGuess);
 
-    //replace placeholder with chosen word
-    if (currentWord.indexOf(userGuess) > -1) {
-        correctGuess.push(userGuess);
+            placeholder[currentWord.indexOf(userGuess)] = userGuess;
+            placeholderhtml[0].innerHTML = placeholder.join(' ');
+            // correctGuesshtml[0].innerHTML = correctGuess.join(' ');
+            console.log(placeholder)
 
-        placeholder[currentWord.indexOf(userGuess)] = userGuess;
-        placeholderhtml[0].innerHTML = placeholder.join(' ');
-        correctGuesshtml[0].innerHTML = correctGuess.join(' ');
-        console.log(placeholder)
-
-        // console.log(correctGuess);
-    }
-    else if (currentWord.indexOf(userGuess) <= -1) {
-        wrongGuess.push(userGuess);
-        wrongGuesshtml[0].innerHTML = wrongGuess.join(' ')
-        console.log(wrongGuess);
-    }
+            // console.log(correctGuess);
+        }
+        else if (currentWord.indexOf(userGuess) <= -1) {
+            wrongGuess.push(userGuess);
+            wrongGuesshtml[0].innerHTML = wrongGuess.join(' ')
+            console.log(wrongGuess);
+        };
+    };
 
     if (placeholder.join(' ') == currentWord) {
         alert("you win!");
