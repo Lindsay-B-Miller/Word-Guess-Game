7bc3d007a4c82370a5661b814b00dcad65762036
Lindsay <lindsay.lbm@gmail.com>
2019-09-19 18:12:02 -0500

    ready besides for loop to accommodate multiple letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,16 @@
 var wordOptions = ["adventure", "animals", "backpack", "campground", "campfire", "cabin", "compass", "equipment", "flashlight", "fishing", "forest", "hammock", "hike", "hiking boots", "bug spray", "lantern", "nature", "outdoors", "outside", "sleeping bag", "sunscreen", "tent", "water bottle", "waterfall"];
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var letters = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 // var buildWord;
 var currentWord;
 var userGuess;
 var wrongGuess = [];
 var correctGuess = [];
 var placeholder = [];
+var guessesLeft = 10;
 var placeholderhtml = document.getElementsByClassName("placeholder");
 var correctGuesshtml = document.getElementsByClassName("correctGuess");
-var wrongGuesshtml = document.getElementsByClassName("wrongGuess");
+// var wrongGuesshtml = document.getElementsByClassName("wrongGuess");
+// var guessesLeft = document.getElementById("#guessesLeft")
 
 
 //computer needs to choose a word
@@ -24,8 +26,21 @@ function generatePlaceholder() {
     return placeholder;
 }
 
+//function for the number of guesses remaining
+function numberGuessesRemain() {
+    document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+};
+
+
+//function for the display of wrong letters
+function wrongLetters() {
+    document.querySelector("#wrongGuess").innerHTML = "Your guesses so far: " + wrongGuess + " ";
+}
+
+
 placeholderhtml[0].innerHTML = generatePlaceholder().join(' ');
 // console.log(generatePlaceholder());
+
 
 //user chooses a letter
 document.onkeyup = function () {
@@ -33,6 +48,8 @@ document.onkeyup = function () {
     console.log(userGuess)
 
     if (letters.indexOf(userGuess) > -1) {
+
+
 
         //replace placeholder with chosen word
         if (currentWord.indexOf(userGuess) > -1) {
@@ -45,12 +62,23 @@ document.onkeyup = function () {
 
             // console.log(correctGuess);
         }
+
+        else if (wrongGuess.includes(userGuess)) {
+            alert("You have already selected this letter. Please try a different letter")
+        }
         else if (currentWord.indexOf(userGuess) <= -1) {
             wrongGuess.push(userGuess);
-            wrongGuesshtml[0].innerHTML = wrongGuess.join(' ')
+            // wrongGuesshtml[0].innerHTML = wrongGuess.join('  ')
+            guessesLeft--;
+            numberGuessesRemain();
+            wrongLetters();
             console.log(wrongGuess);
         };
+
+
     };
+
+
 
     if (placeholder.join(' ') == currentWord) {
         alert("you win!");
@@ -60,3 +88,4 @@ document.onkeyup = function () {
 };
 
 
+document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
---
+++ assets/javascript/game2.js
@@ -0,0 +1,100 @@
+var wordOptions = ["adventure", "animals", "backpack", "campground", "campfire", "cabin", "compass", "equipment", "flashlight", "fishing", "forest", "hammock", "hike", "hiking boots", "bug spray", "lantern", "nature", "outdoors", "outside", "sleeping bag", "sunscreen", "tent", "water bottle", "waterfall"];
+var letters = [" ", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+// var buildWord;
+var currentWord;
+var userGuess;
+var wrongGuess = [];
+var correctGuess = [];
+var placeholder = [];
+var guessesLeft = 10;
+var alreadyTried = [];
+var placeholderhtml = document.getElementsByClassName("placeholder");
+var correctGuesshtml = document.getElementsByClassName("correctGuess");
+var wrongGuesshtml = document.getElementsByClassName("wrongGuess");
+
+
+
+//computer needs to choose a word
+var currentWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+console.log(currentWord)
+
+//create placeholder for letters in chosen word 
+function generatePlaceholder() {
+    for (var i = 0; i < currentWord.length; i++) {
+        placeholder.push('_');
+
+    }
+    return placeholder;
+}
+
+placeholderhtml[0].innerHTML = generatePlaceholder().join(' ');
+// console.log(generatePlaceholder());
+
+
+//user chooses a letter
+document.onkeyup = function () {
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    console.log(userGuess)
+
+
+    if (letters.indexOf(userGuess) > -1) {
+        if (guessesLeft > 0) {
+
+        }
+        // Makes sure user hasn't already guessed letter
+        if (alreadyTried.includes(userGuess)) {
+            alert("You have already selected this letter. Please try a different letter")
+        }
+
+        function evaluateGuess(userGuess) {
+            var positions = [];
+            for (var i = 0; i < currentWord.length; i++) {
+                if (currentWord[i] === userGueses) {
+                    positions.push[i];
+                    console.log(userGuess)
+                }
+            }
+            if (positions.length <= 0) {
+                guessesLeft--;
+            }
+            else {
+                for (var a = 0; a < positions.length; a++) {
+                    placeholder[positions[a]] = userGuess;
+                }
+            }
+        }
+
+
+
+    };
+};
+
+
+
+    //     //replace placeholder with chosen word
+    //     if (currentWord.indexOf(userGuess) > -1) {
+    //         correctGuess.push(userGuess);
+
+    //         placeholder[currentWord.indexOf(userGuess)] = userGuess;
+    //         placeholderhtml[0].innerHTML = placeholder.join(' ');
+    //         // correctGuesshtml[0].innerHTML = correctGuess.join(' ');
+    //         console.log(placeholder)
+
+    //         // console.log(correctGuess);
+    //     }
+
+
+    //     else if (currentWord.indexOf(userGuess) <= -1) {
+    //         wrongGuess.push(userGuess);
+    //         wrongGuesshtml[0].innerHTML = wrongGuess.join(' ')
+    //         console.log(wrongGuess);
+    //     };
+    // };
+
+
+
+    // if (placeholder.join(' ') == currentWord) {
+    //     alert("you win!");
+    // };
+
+
